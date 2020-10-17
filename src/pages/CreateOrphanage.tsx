62b55493a7be2994b1f7f8d822d524bd3080fb2f
Héber Silvério <hebersilverio@gmail.com>
2020-10-17 18:02:22 -0300

    terminou os bugs

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -10,13 +10,6 @@ import mapIcon from "../utils/mapIcon";
 
 import '../styles/pages/create-orphanage.css';
 
-const happyMaIcon = L.icon({
-  iconUrl: mapMarkerImg,
-
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
 
 export default function CreateOrphanage() {
   return (
